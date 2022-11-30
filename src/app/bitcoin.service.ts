55e0e6d30e8bfabc605b8da2f993d4964dabdf70
CaioCroccia22 <118224318+CaioCroccia22@users.noreply.github.com>
2022-11-29 22:59:59 -0300

    1 file modified

--- src/app/bitcoin.service.ts
+++ src/app/bitcoin.service.ts
@@ -9,7 +9,18 @@ export class BitcoinService {
 
   constructor(private http: HttpClient) {}
 
+
+  /*
+Isso envia uma solicitação HTTP GET para a API para obter uma lista de pacotes que pertencem ao @angularescopo e, em seguida, atribui o totalretornado na resposta à propriedade local totalAngularPackages. O tipo de response é definido para <any>lidar com quaisquer propriedades retornadas na resposta.
+  */
   public getCoin(){
-    return this.http.get(`${this.SERVER_URL}/`)
+    return this.http.get<any>('https://api.coindesk.com/v1/bpi/currentprice.json').subscribe
+
+    /*
+    Solicitação GET com resposta fortemente tipada
+    Isso envia a mesma solicitação acima, mas define o tipo de resposta para uma SearchResultsinterface personalizada que define as propriedades de resposta esperadas.
+    */ 
+
+    //https://jasonwatmore.com/post/2019/09/06/angular-http-get-request-examples
   }
  }
